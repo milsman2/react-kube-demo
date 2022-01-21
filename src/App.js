90c1bdf81708de4013749530f211411dea321c80
milsman2 <milsman2@gmail.com>
2022-01-21 05:46:54 +0000

    final message update

--- src/App.js
+++ src/App.js
@@ -5,13 +5,33 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>
-          To those of you here to see the end product of my blog post, thank you.
+          Those here to see the end product of my blog post, thank you.
         </h1>
         <h2>
           But I would also like to set aside part of my deployed lab to thanking a few specifically.
         </h2>
         <p>
-          Most of all, thank you to my wife, Rachel. 
+          Most of all, thank you to my wife, Rachel. The only way I could have gotten this far is with a wife who is willing
+          to know what it means and be supportive when you say "There's something up with the DNS, my server is down so do you
+          mind if I run upstairs real quick?" Thank you for listening to me try to explain what I think is wrong in the lab. 
+          There's a lot to be said for that kind of support.
+        </p>
+        <p>
+          Thank you for to my parents, Marcie and Leslie. Mom and Pop, thank you for always wanting me to learn. For reading to me when I was younger. 
+          I can't really say enough about how possible you have made everything by fostering a home that values growth and creativity.
+        </p>
+        <p>
+          David Levine. Thanks for challenging me. For knowing how to take sarcastic ideas and make them a realistic goal for me to learn and reach towards.
+        </p>
+        <p>
+          Kyle. For encouraging me in ways that go beyond programming and IT. For many times being so good at things that I look up
+          and want to teach myself these kinds of skills to keep up with. For always wanting to be my teammate.
+        </p>
+        <p>
+          Grandma. For being responsible for basically our whole family and a beacon of wisdom my whole life.
+        </p>
+        <p>
+          Thank you to everyone else who I don't have enough screen space for on this page right now.
         </p>
         <a
           className="App-link"
@@ -20,6 +40,9 @@ function App() {
         >
           blog.kanebroslab.com
         </a>
+        <h2>
+          - Miles // 1.20.22
+        </h2>
       </header>
     </div>
   );
